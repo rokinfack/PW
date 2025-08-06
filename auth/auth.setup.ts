4be0b7505978f86e8f07bcd39cb6bce3346ace0b
rostand kinfack <kinfackrostand@gmail.com>
2025-08-06 21:43:53 +0200

    test

--- auth/auth.setup.ts
+++ auth/auth.setup.ts
@@ -1,26 +1,30 @@
 import { test as setup } from '@playwright/test';
 import * as dotenv from 'dotenv';
 
-dotenv.config(); // Charge .env
+dotenv.config();
 
-console.log('USERNAME:', process.env.GITHUB_USERNAME);
-console.log('PASSWORD:', process.env.GITHUB_PASSWORD);
+const username = process.env.GITHUB_USERNAME;
+const password = process.env.GITHUB_PASSWORD;
+
+if (!username || !password) {
+  throw new Error('GITHUB_USERNAME or GITHUB_PASSWORD is not defined');
+}
 
 const adminFile = 'playwright/.auth/admin.json';
 const userFile = 'playwright/.auth/user.json';
 
 setup('authenticate as admin', async ({ page }) => {
   await page.goto('https://github.com/login');
-  await page.getByLabel('Username or email address').fill(process.env.GITHUB_USERNAME!);
-  await page.getByLabel('Password').fill(process.env.GITHUB_PASSWORD!);
+  await page.getByLabel('Username or email address').fill(username);
+  await page.getByLabel('Password').fill(password);
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.context().storageState({ path: adminFile });
 });
 
 setup('authenticate as user', async ({ page }) => {
   await page.goto('https://github.com/login');
-  await page.getByLabel('Username or email address').fill(process.env.GITHUB_USERNAME!);
-  await page.getByLabel('Password').fill(process.env.GITHUB_PASSWORD!);
+  await page.getByLabel('Username or email address').fill(username);
+  await page.getByLabel('Password').fill(password);
   await page.getByRole('button', { name: 'Sign in' }).click();
   await page.context().storageState({ path: userFile });
 });
